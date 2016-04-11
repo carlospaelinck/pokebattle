cb433968b6b9f4401fe0780010cc48f807c1a47f
Carlos Paelinck <carlos@carlospaelinck.com>
2016-04-11 09:53:29 -0500

    Initial Commit

---
+++ PokéBattle/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PokéBattle
+//
+//  Created by Carlos Paelinck on 4/11/16.
+//  Copyright © 2016 Carlos Paelinck. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PokéBattle/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PokéBattle
+//
+//  Created by Carlos Paelinck on 4/11/16.
+//  Copyright © 2016 Carlos Paelinck. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
